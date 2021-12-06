81b74d051dd3666816a98166944a0e29ffc18dce
farsider350 <farsider350@github.com>
2021-12-07 06:57:51 +1000

    dirs

--- p2pool/bitcoin/networks/creditcoin.py
+++ p2pool/bitcoin/networks/creditcoin.py
@@ -19,8 +19,8 @@ RPC_CHECK =RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 150 # s
-SYMBOL = ''
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Creditcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Creditcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.creditcoin'), 'creditcoin.conf')
+SYMBOL = 'CRED'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'credits') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/credits/') if platform.system() == 'Darwin' else os.path.expanduser('~/.credits'), 'credits.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://'
 TX_EXPLORER_URL_PREFIX = 'https://'
