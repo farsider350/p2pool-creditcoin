4e0a283e65cabb378e48c4ecd8a71ef4285945dc
farsider350 <farsider350@github.com>
2021-12-06 19:23:51 +1000

    CreditCoin

---
+++ p2pool/bitcoin/networks/creditcoin.py
@@ -0,0 +1,29 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'f1c117dc'.decode('hex')
+P2P_PORT = 6666
+ADDRESS_VERSION = 28
+RPC_PORT = 9332
+RPC_CHECK =RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+#            'creditcoin' in (yield bitcoind.rpc_help()) 
+            (yield helper.check_block_header(bitcoind, 'd3602f1423e127f8f1755de875f48edb6bbf5c4b1306dd4c6df85af344def718')) and
+                          (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
+        ))
+SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+BLOCK_PERIOD = 150 # s
+SYMBOL = ''
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Creditcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Creditcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.creditcoin'), 'creditcoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://'
+TX_EXPLORER_URL_PREFIX = 'https://'
+SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
+DUMB_SCRYPT_DIFF = 2**16
+DUST_THRESHOLD = 0.03e8
---
+++ p2pool/networks/creditcoin.py
@@ -0,0 +1,25 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['creditcoin']
+SHARE_PERIOD = 20 # seconds
+CHAIN_LENGTH = 2*24*60 # shares
+REAL_CHAIN_LENGTH = 2*24*60 # shares
+TARGET_LOOKBEHIND = 60 # shares
+SPREAD = 8 # blocks
+IDENTIFIER = 'e037d5b8c6923310'.decode('hex')
+PREFIX = '7208c1a53ef529b0'.decode('hex')
+P2P_PORT = 3011
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 3012
+BOOTSTRAP_ADDRS = '139.180.182.227'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-creditcoin'
+VERSION_CHECK = lambda v: None if 170401 <= v else 'AustraliaCash version too old. Upgrade to 0.17.4.1 or newer!'
+VERSION_WARNING = lambda v: None
+SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
+MINIMUM_PROTOCOL_VERSION = 3301
+NEW_MINIMUM_PROTOCOL_VERSION = 3301
+SEGWIT_ACTIVATION_VERSION = 17
+BLOCK_MAX_SIZE = 1000000
+BLOCK_MAX_WEIGHT = 4000000
